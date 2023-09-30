d8f16bdf590d8aaf9ee195489b1a3d0bc6b69cbe
Angelica Beltran <angelica.beltran11@gmail.com>
2023-09-30 16:35:21 -0500

    planning outline for details component

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,24 @@
+
+//bring in MATERIAL UI FOR STUFF LIKE BUTTONS 
+// TODO: When a movie poster is clicked, a user should be brought to the /details view for that movie.
+// -we'd have to do the route to /details
+// -the <p> that shows the details
+// - writing code that goes to GET? the details from the database?
+// - we'd be creating an onClick for the movie image 
+// - we'd be writing the onClick function 
+// - a space for the genres, title, and description
+// - a back to list BUTTON
+// - setting up the button to take us back to the /Home/List Page
+
+
+
+export default function Details(){
+
+    return (
+        <>
+        <h1>HERE I WOULD WANT THE DETAILS TO APPEAR FOR EACH MOVIE OR THE ONE SELECTED? </h1>
+        <h1>PER THE WIREFRAME IT WOULD BE DETAILS FOR ALL </h1>
+        
+        </>
+        )
+}
